0b4061b8912f3962cc4aebf1b7152596dd6d57ea
Vitavanika <vitavanika@gmail.com>
2025-09-11 15:13:55 +0200

    fix createOrder in controller

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -1,19 +1,62 @@
 import * as ordersService from '../services/ordersService.js';
 import sendResponse from '../utils/sendResponse.js';
+import { Order } from '../models/Order.js';
+import { Flower } from '../models/Flower.js';
 
-export const createOrder = async (req, res, next) => {
+export const createOrder = async (req, res) => {
   try {
-    const order = await ordersService.createOrder(req.body);
+    const { customer, flowers } = req.body;
+    const flowerIds = flowers.map((item) => item.flowerId);
+    const flowerDetails = await Flower.find({ _id: { $in: flowerIds } }).select(
+      'name price image',
+    );
+
+    let totalPrice = 0;
+    const flowersForOrder = [];
+
+    for (const item of flowers) {
+      const flower = flowerDetails.find(
+        (d) => d._id.toString() === item.flowerId,
+      );
+      if (!flower) {
+        return sendResponse(res, {
+          code: 404,
+          message: `Flower with ID ${item.flowerId} not found.`,
+        });
+      }
+      totalPrice += flower.price * item.quantity;
+      flowersForOrder.push({
+        flowerId: flower._id,
+        quantity: item.quantity,
+      });
+    }
+
+    const newOrder = new Order({
+      customer,
+      flowers: flowersForOrder,
+      totalPrice,
+    });
+
+    const savedOrder = await newOrder.save();
+
+    const populatedOrder = await Order.findById(savedOrder._id).populate({
+      path: 'flowers.flowerId',
+      select: 'name price image',
+    });
+
     return sendResponse(res, {
       code: 201,
       message: 'Order created successfully',
-      data: order,
+      data: populatedOrder,
     });
-  } catch (err) {
-    next(err);
+  } catch (error) {
+    return sendResponse(res, {
+      code: 400,
+      message: 'Failed to create order',
+      error: error.message,
+    });
   }
 };
-
 export const getOrderById = async (req, res, next) => {
   try {
     const order = await ordersService.getOrderById(req.params.id);
